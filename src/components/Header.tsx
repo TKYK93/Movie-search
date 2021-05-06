0e4d3efc11cb3e3f00e3b34776bbc90e25371124
TKYK93 <laylalayla2905@gmail.com>
2021-05-06 22:14:58 +0200

    typo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,16 @@
-import React, { useState } from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
-import { makeStyles } from '@material-ui/core'
-import SearchBar from './SearchBar'
+import React, { useState } from "react"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import Typography from "@material-ui/core/Typography"
+import IconButton from "@material-ui/core/IconButton"
+import MenuIcon from "@material-ui/icons/Menu"
+import { makeStyles } from "@material-ui/core"
+import SearchBar from "./SearchBar"
 
 const useStyles = makeStyles(() => ({
   appBar: {
-    minHeight: '60px',
+    minHeight: "60px",
   },
-
 }))
 
 interface HeaderProps {
@@ -22,23 +21,14 @@ const Header: React.FC<HeaderProps> = ({ title }) => {
   const classes = useStyles()
 
   return (
-      <AppBar position="static" className={classes.appBar}>
-        <Toolbar variant="dense">
-          <IconButton
-            edge="start"
-            className="header.menuButton"
-            color="inherit"
-            aria-label="menu"
-
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit">
-            {title}
-          </Typography>
-          <SearchBar />
-        </Toolbar>
-      </AppBar>
+    <AppBar position="static" className={classes.appBar}>
+      <Toolbar variant="dense">
+        <Typography variant="h6" color="inherit">
+          {title}
+        </Typography>
+        {title !== "Detail" && <SearchBar />}
+      </Toolbar>
+    </AppBar>
   )
 }
 
