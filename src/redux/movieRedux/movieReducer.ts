f20f00eecd93d9e12e6eea3ff64ba28be324a650
TKYK93 <laylalayla2905@gmail.com>
2021-05-06 05:05:11 +0200

    stylied & function to call API for detailed info

--- src/redux/movieRedux/movieReducer.ts
+++ src/redux/movieRedux/movieReducer.ts
@@ -4,12 +4,14 @@ import { Movie, MovieDetail } from '../../models/Movie'
 
 export interface MovieState {
   searchedMovies: Movie[]
-  movieDetail: MovieDetail 
+  movieDetail: MovieDetail
+  selectedCountry: string
 }
 
 export const initialState: MovieState = {
   searchedMovies: [],
-  movieDetail: {} as MovieDetail
+  movieDetail: {} as MovieDetail,
+  selectedCountry: "US"
 }
 
 export const MovieReducer = (state = initialState, action: MovieActions) => {
@@ -17,9 +19,18 @@ export const MovieReducer = (state = initialState, action: MovieActions) => {
     case 'GET_SEARCHED_MOVIES':
       return { ...state, searchedMovies: action.movies }
 
+    case 'CLEAR_SEARCHED_MOVIES':
+      return { ...state, searchedMovies: initialState.searchedMovies }
+
     case 'GET_MOVIE_DETAIL':
       return { ...state, movieDetail: action.movieDetail }
-      
+
+    case 'CLEAR_MOVIE_DETAIL':
+      return { ...state, movieDetail: initialState.movieDetail }
+
+    case 'SET_COUNTRY':
+      return { ...state, selectedCountry: action.country }
+
     default:
       return state
   }
