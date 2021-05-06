c3a970c2d44ae81dd6da5b2df5dfb206fdb2e79d
TKYK93 <laylalayla2905@gmail.com>
2021-05-06 22:14:47 +0200

    added backButton handler

--- src/pages/SearchResult.tsx
+++ src/pages/SearchResult.tsx
@@ -1,19 +1,14 @@
 import { Grid, IconButton, makeStyles } from "@material-ui/core"
 import { ArrowBackIosOutlined } from "@material-ui/icons"
-import React, { useEffect, useState } from "react"
-import { useSelector } from "react-redux"
+import React from "react"
+import { useDispatch, useSelector } from "react-redux"
 import { useHistory } from "react-router"
 import Header from "../components/Header"
 import MovieCard from "../components/MovieCard"
-import { Movie } from "../models/Movie"
+import { clearSearchedMovies } from "../redux/movieRedux/movieActions"
 import { AppState } from "../redux/store"
 
 const useStyles = makeStyles({
-  Home: {
-    // marign: 0,
-    // padding: 0,
-    // boxSizing: "border-box",
-  },
   searchMoviesWrapper: {
     padding: "0 3%",
   },
@@ -26,17 +21,23 @@ const useStyles = makeStyles({
 
 const SearchedResult: React.FC = () => {
   const classes = useStyles()
+  const dispatch = useDispatch()
   const history = useHistory()
   const searchedMoviesList = useSelector(
     (state: AppState) => state.movieState.searchedMovies
   )
 
+  const backButtonHandler = () => {
+    dispatch(clearSearchedMovies())
+    history.replace("/home")
+  }
+
   return (
-    <div className={classes.Home}>
-      <Header title="SearchedResult" />
+    <div className="SearchResult">
+      <Header title="Search Result" />
       {searchedMoviesList.length > 0 ? (
         <div className={classes.searchMoviesWrapper}>
-          <IconButton edge="start" onClick={() => history.push("/home")}>
+          <IconButton edge="start" onClick={() => backButtonHandler()}>
             <ArrowBackIosOutlined />
             back to home
           </IconButton>
@@ -44,7 +45,7 @@ const SearchedResult: React.FC = () => {
           <Grid container justify="center" spacing={3}>
             {searchedMoviesList.map((searchedMovie, index) => (
               <Grid key={index} item xs={12} sm={6} md={4}>
-                <MovieCard {...searchedMovie} />
+                <MovieCard {...searchedMovie} purpose="detail" />
               </Grid>
             ))}
           </Grid>
